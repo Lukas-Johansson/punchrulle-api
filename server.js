9dee431745a1c5410a35e7d63972435349db1da2
= <=>
2024-02-15 14:24:07 +0100

    Fix punchrullar count and add functionality

--- server.js
+++ server.js
@@ -18,36 +18,47 @@ app.use(cors());
 
 app.post('/punchrullar', async (req, res) => {
     console.log(req)
+
+
+    let punchrulleCount = -1
+    try {
+        [result] = await pool.promise().query('SELECT remaining FROM punchrullar ORDER BY timetable DESC LIMIT 1')
+        console.log(result)
+        punchrulleCount = result[0].remaining
+
+    } catch (error) {
+        console.error(error)
+    }
+
     if (req.body.text) {
         console.log(req.body.text)
         const [command, ...args] = req.body.text.split(' ')
-        console.log(command)
+        console.log(command === 'add')
         console.log(args)
 
-        if (command.toLowercase === 'add') {
+        if (command.toLowerCase() === 'add') {
             console.log('punchrullar add')
             // sql query add 1 rulle
+            // const remainingChange = action === 'add' ? currentRemaining + 1 : Math.max(0, currentRemaining - 1);
+            if (punchrulleCount >= 0) {
+                let updateAmount = args[0] ? parseInt(args[0]) : 1
+                const sqlQuery = 'INSERT INTO punchrullar (remaining, timetable) VALUES (?, CURRENT_TIMESTAMP)';
+                update = await pool.promise().query(sqlQuery, [punchrulleCount + updateAmount]);
+                console.log(update)
+                punchrulleCount += updateAmount
+            }
+            
+
         } else if (command.toLowercase === 'remove') {
             console.log('punchrullar remove')
             // sql query remove 1 rulle
         }
     }
-    // const moj = req.body.text.split(' ')
-    // console.log(moj)
 
-    //     if (moj[0] === 'punchrullar' && moj[1] === 'count') {
-    //         console.log('punchrullar count')
-    //     }
-
-    try {
-
-        const [result] = await pool.promise().query('SELECT remaining FROM punchrullar ORDER BY timetable DESC LIMIT 1')
-
-        console.log(result)
-        res.json(result)
-    } catch (error) {
-        console.error(error)
-    }
+    res.json({
+        response_type: 'in_channel',
+        text: `Punchrullar: ${punchrulleCount}`
+    })
 
 })
 
